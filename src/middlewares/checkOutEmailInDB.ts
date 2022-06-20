0fa7be51cd9ff525f5b1057aab79544d47c30901
SergeHall <sergioartg@gmail.com>
2022-06-20 04:55:07 -0700

    refactoring export async function checkOutEmailOrLoginInDB

--- src/middlewares/checkOutEmailInDB.ts
+++ src/middlewares/checkOutEmailInDB.ts
@@ -3,21 +3,29 @@ import {ioc} from "../IoCContainer";
 
 
 export async function checkOutEmailOrLoginInDB(req: Request, res: Response, next: NextFunction) {
-  const email = req.body.email
-  const login = req.body.login
+  const email = req.body.email;
+  const login = req.body.login;
+  let errorMessage = {};
   if (email || login) {
     const checkOutEmailInDB = await ioc.authUsersAccountService.findByLoginAndEmail(email, login);
     if (checkOutEmailInDB === null) {
       next()
       return
     }
+    if (checkOutEmailInDB.accountData.email !== email) {
+      errorMessage = {
+        message: "Invalid email.",
+        field: "email"
+      }
+    }
+    if (checkOutEmailInDB.accountData.login !== login) {
+      errorMessage = {
+        message: "Invalid login.",
+        field: "login"
+      }
+    }
     res.status(400).send({
-      "errorsMessages": [
-        {
-          message: "That username is taken. Try another.",
-          field: "login"
-        }
-      ]
+      errorsMessages: [errorMessage]
     });
     return
   }
