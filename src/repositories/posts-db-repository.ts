aebc840a88721363975fffc27e609988f1254dc6
SergeHall <sergioartg@gmail.com>
2022-09-22 00:53:40 -0700

    fixed getCommentsByPostId add query sortBy=&sortDirection=

--- src/repositories/posts-db-repository.ts
+++ src/repositories/posts-db-repository.ts
@@ -210,21 +210,6 @@ export class PostsRepository {
 
     const pagesCount = Math.ceil(totalCount / pageSize)
 
-    let comments = await MyModelComments.findOne(filter, {
-      _id: false
-    }).lean()
-      .then(comments => comments?.allComments.sort(byField(field, asc, desc)).slice(startIndex, startIndex + pageSize))
-
-
-    if (!comments) {
-      comments = []
-    }
-
-    // @ts-ignore
-    const allCommentsDelMongoId = comments.map(({_id, ...rest}) => {
-      return rest;
-    });
-
     let desc = 1
     let asc = -1
     let field = "createdAt"
@@ -242,18 +227,59 @@ export class PostsRepository {
     console.log("asc = ", asc)
     console.log("desc = ", desc)
 
-    // allCommentsDelMongoId.sort(byField(field, asc, desc))
+    // comments.sort(byField(field, asc, desc))
     // sort array comments
     function byField(field: string, asc: number, desc: number) {
       return (a: any, b: any) => a[field] > b[field] ? asc : desc;
     }
+
+    let comments = await MyModelComments.findOne(filter, {
+      _id: false, 'allComments._id': false
+    }).lean()
+      // .then(comments => comments?.allComments.slice(startIndex, startIndex + pageSize))
+    .then(comments => comments?.allComments.sort(byField(field, asc, desc)))
+
+    if (!comments) {
+      comments = []
+    }
+
+    // @ts-ignore
+    // const allCommentsDelMongoId = comments.map(({_id, ...rest}) => {
+    //   return rest;
+    // });
+
+    // let desc = 1
+    // let asc = -1
+    // let field = "createdAt"
+    //
+    // if (sortDirection === "asc") {
+    //   desc = -1
+    //   asc = 1
+    // }
+    //
+    // if (sortBy === "userId" || sortBy === "userLogin" || sortBy === "content") {
+    //   field = sortBy
+    // }
+    //
+    // console.log("field = ", field)
+    // console.log("asc = ", asc)
+    // console.log("desc = ", desc)
+    //
+    // // comments.sort(byField(field, asc, desc))
+    // // sort array comments
+    // function byField(field: string, asc: number, desc: number) {
+    //   return (a: any, b: any) => a[field] > b[field] ? asc : desc;
+    // }
+
+    comments.slice(startIndex, startIndex + pageSize)
+
 
     return {
       pagesCount: pagesCount,
       page: pageNumber,
       pageSize: pageSize,
       totalCount: totalCount,
-      items: allCommentsDelMongoId
+      items: comments
     };
   }
 
