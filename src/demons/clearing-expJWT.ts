eb2e0ee29127a9575e0c6a75c21ec743c7c41f67
SergeHall <sergioartg@gmail.com>
2022-10-22 00:03:02 -0700

    test9 change clearingExpDateJWT

--- src/demons/clearing-expJWT.ts
+++ src/demons/clearing-expJWT.ts
@@ -2,10 +2,9 @@ import {MyModelDevicesSchema} from "../mongoose/DevicesSchemaModel";
 
 
 export const clearingExpDateJWT = async () => {
-  // runs every 2 sec
+  // runs every 1 sec
   setTimeout(async () => {
-    const currentDate = new Date().toISOString()
-    await MyModelDevicesSchema.deleteMany({"expirationDate": {$lt: currentDate}})
+    await MyModelDevicesSchema.deleteMany({"expirationDate": {$lt: new Date().toISOString()}})
     await clearingExpDateJWT()
-  }, 2000)
+  }, 1000)
 }
