e697ec807df15fb00d72d93fb2fae0ac3dac28df
sergehall <sergioartg@gmail.com>
2022-11-01 20:56:29 -0700

    add replaced noneStatusRefreshToken to noneStatusAccessToken

--- src/repositories/preparation-comments.ts
+++ src/repositories/preparation-comments.ts
@@ -18,13 +18,13 @@ export class PreparationComments {
       const comment = commentsArray[i]
       const commentId = commentsArray[i].id
       const filterCommentId = {commentId: commentId}
-
+      console.log(comment, 'comment')
       const checkCurrentLikeStatus = await MyModelLikeStatusCommentId.findOne(
         {
           $and: [filterCurrentUserId, filterCommentId]
         }
       )
-
+      console.log(checkCurrentLikeStatus, "checkCurrentLikeStatus")
       if (checkCurrentLikeStatus) {
         currentLikeStatus = {likeStatus: checkCurrentLikeStatus.likeStatus}
       }
@@ -41,6 +41,18 @@ export class PreparationComments {
             {likeStatus: "Dislike"}]
       }).lean()
 
+      console.log("-------------------------------", {
+        id: comment.id,
+        content: comment.content,
+        userId: comment.userId,
+        userLogin: comment.userLogin,
+        createdAt: comment.createdAt,
+        likesInfo: {
+          likesCount: countLikes,
+          dislikesCount: countDislike,
+          myStatus: currentLikeStatus.likeStatus,
+        }
+      }, "-------------------------------")
       filledCommentsArray.push({
         id: comment.id,
         content: comment.content,
